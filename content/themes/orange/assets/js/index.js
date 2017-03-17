3da408d070ccd9d6ea479320ffe0ebfb84e9e614
MeynL <724610695@qq.com>
2017-03-17 17:23:11 +0800

    add new theme

---
+++ content/themes/orange/assets/js/index.js
@@ -0,0 +1,16 @@
++function(){
+    $(window).on("scroll",function(){
+        $(window).scrollTop()>80? navf():navd();
+    });
+    function navf(){
+        $(".nav").addClass("float");
+        $(".cont").css("margin-top","74px");
+    }
+    function navd(){
+        $(".nav").removeClass("float");
+        $(".cont").css("margin-top","20px");
+    }
+    $("#slect-input").ghostHunter({
+        results:"#result"
+    });
+}();
